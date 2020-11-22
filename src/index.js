8028eb3f63156d790234fb102c3545ed4957acac
Natatashkin <cs070283sng@gmail.com>
2020-11-22 18:37:32 +0200

    add files

---
+++ src/index.js
@@ -0,0 +1,70 @@
+import './styles.css';
+import 'material-design-icons/iconfont/material-icons.css';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+import { notification } from './js/notify';
+import ApiService from './js/apiService';
+import getRefs from './js/getRefs';
+import photoCardTpl from './templates/photo-card.hbs';
+// import loadMoreBtn from './templates/loadMoreBtn.hbs';
+import LoadMoreButton from './js/loadMoreBtn';
+
+
+
+
+const loadBtn = new LoadMoreButton({
+    selector: '.load-more-btn'
+})
+
+const refs = getRefs();
+
+refs.searchForm.addEventListener("submit", onSearch);
+loadBtn.refs.button.addEventListener('click', fetchImages);
+
+
+const apiService = new ApiService();
+
+
+function onSearch(event) {
+    event.preventDefault();
+    apiService.inputQuery = event.target.elements.query.value.trim();
+    if (apiService.inputQuery === '') {
+        notification('error', 'Ты забыл написать что ищешь!');
+        return;
+    }
+
+    loadBtn.showBtn();
+    apiService.resetPage();
+    clearGallery();
+    fetchImages();
+}
+
+
+function renderGallery(hits) {
+    refs.imagesContainer.insertAdjacentHTML('beforeend', photoCardTpl(hits))
+}
+
+
+function clearGallery() {
+    refs.imagesContainer.innerHTML = '';
+} 
+
+function fetchImages() {
+    loadBtn.disableBtn();
+    apiService.getQuery().then(hits => { 
+        if (hits.length === 0) {
+            notification('error', 'А бредовее запроса ты не мог придумать?');
+            loadBtn.hideBtn();
+            return;
+        }
+       
+        if (hits.length > 0) {
+            renderGallery(hits)
+            loadBtn.enableBtn();
+            return;
+        }
+    });
+}
+
+
+
---
+++ src/js/apiService.js
@@ -0,0 +1,43 @@
+const API_KEY = '14467768-9171c4f16b15a9d8391496270';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export default class ApiService {
+
+    constructor() {
+        this.searchQuery = '';
+        this.page = 1;
+
+    }
+
+    getQuery() {
+
+        return fetch(`${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => { 
+                this.incrementPage();
+                return data.hits
+            })
+            .catch(error => {
+            console.log(error);
+        })
+    }
+
+    incrementPage() {
+        this.page += 1;
+    }
+
+    resetPage() {
+        this.page = 1;
+    }
+
+    get inputQuery() {
+        return this.searchQuery; 
+    }
+
+    set inputQuery(newInputQuery) {
+        this.searchQuery = newInputQuery;
+    }
+}
+
