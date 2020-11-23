f74c2506209d036bdcd6a9472d5d8193677b16fd
Natatashkin <cs070283sng@gmail.com>
2020-11-23 22:38:13 +0200

    upd

---
+++ src/js/onSearch.js
@@ -0,0 +1,54 @@
+import getRefs from './getRefs';
+import ApiService from './apiService';
+import photoCardTpl from '../templates/photo-card.hbs';
+import { loadBtn } from './loadButton';
+import { notification } from './notify';
+
+
+const refs = getRefs();
+console.log(refs);
+
+refs.searchForm.addEventListener("submit", onSearch);
+
+export const apiService = new ApiService();
+
+function onSearch(event) {
+    event.preventDefault();
+    apiService.inputQuery = event.target.elements.query.value.trim();
+    if (apiService.inputQuery === '') {
+        notification('error', 'Ты забыл написать что ищешь!');
+        return;
+    }
+
+    loadBtn.showBtn();
+    apiService.resetPage();
+    clearGallery();
+    fetchImages();
+}
+
+
+function renderGallery(hits) {
+    refs.imagesContainer.insertAdjacentHTML('beforeend', photoCardTpl(hits))
+}
+
+
+function clearGallery() {
+    refs.imagesContainer.innerHTML = '';
+} 
+
+export function fetchImages() {
+    loadBtn.disableBtn();
+    apiService.getQuery().then(hits => { 
+        if (hits.length === 0) {
+            notification('error', 'А бредовее запроса ты не мог придумать?');
+            loadBtn.hideBtn();
+            return;
+        }
+       
+        if (hits.length > 0) {
+            renderGallery(hits)
+            loadBtn.enableBtn();
+        }
+        
+    });
+}
